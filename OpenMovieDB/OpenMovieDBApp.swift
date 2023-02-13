229b46e7d8c5c3ab4f13235c9a4fae72dce54cff
devanshuchaudhary <devanshu88@gmail.com>
2023-02-14 00:59:50 +0530

    Initial Commit

---
+++ OpenMovieDB/OpenMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMovieDBApp.swift
+//  OpenMovieDB
+//
+//  Created by dddiehard on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
